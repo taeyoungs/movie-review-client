addd6d01a0bd9f2042add6ce88979ccc72bd04b3
taeyoungs <xoxodudwkd@gmail.com>
2021-02-20 16:47:38 +0900

    [컴포넌트 수정] SearchBarContent style 분리

---
+++ src/products/SearchBarContent/WithEmotion.ts
@@ -0,0 +1,46 @@
+import styled from '@emotion/styled';
+
+const ItemContainer = styled.li`
+  width: 100%;
+  display: flex;
+  padding: 5px;
+  &:hover {
+    background: rgba(255, 255, 255, 0.08);
+  }
+  box-sizing: border-box;
+`;
+
+const PosterContainer = styled.div`
+  width: 4rem;
+  min-width: 4rem;
+  height: calc(4rem * 1.5);
+`;
+
+const Poster = styled.div<{ imgPath?: string }>`
+  background: url(${(props) => props.imgPath});
+  background-size: cover;
+  background-position: center;
+  width: 100%;
+  height: 100%;
+`;
+
+const ContentContainer = styled.div`
+  width: 100%;
+  margin-left: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+`;
+
+const RatingContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export {
+  ItemContainer,
+  PosterContainer,
+  Poster,
+  ContentContainer,
+  RatingContainer,
+};
--- src/products/SearchBarContent/index.tsx
+++ src/products/SearchBarContent/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
-import styled from '@emotion/styled';
 import P from 'components/atoms/P';
 import { ColorPalette } from 'models/color';
 import Icon from 'Icon/Icon';
+import {
+  ContentContainer,
+  ItemContainer,
+  Poster,
+  PosterContainer,
+  RatingContainer,
+} from './WithEmotion';
 
 export interface ISearchProps {
   id: number;
@@ -17,43 +23,6 @@ export interface ISearchProps {
 }
 
 // ToDo: Link
-
-const ItemContainer = styled.li`
-  width: 100%;
-  display: flex;
-  padding: 5px;
-  &:hover {
-    background: rgba(255, 255, 255, 0.08);
-  }
-  box-sizing: border-box;
-`;
-
-const PosterContainer = styled.div`
-  width: 4rem;
-  min-width: 4rem;
-  height: calc(4rem * 1.5);
-`;
-
-const Poster = styled.div<{ imgPath?: string }>`
-  background: url(${(props) => props.imgPath});
-  background-size: cover;
-  background-position: center;
-  width: 100%;
-  height: 100%;
-`;
-
-const ContentContainer = styled.div`
-  width: 100%;
-  margin-left: 20px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
-`;
-
-const RatingContainer = styled.div`
-  display: flex;
-  align-items: center;
-`;
 
 const SearchBarContent: React.FC<ISearchProps> = ({
   id,
